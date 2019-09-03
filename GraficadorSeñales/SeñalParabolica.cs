39700379e4b5959489cb1db8675abab491106d63
david arturo preciado osuna <arturo_preciado@live.com.mx>
2019-09-03 11:08:01 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalParabolica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalParabolica
+    {
+        public List<Muestra> Muestras
+            { get; set; }
+
+        public SeñalParabolica()
+        {
+            Muestras = 
+                new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            resultado = (tiempo * tiempo * tiempo) ;
+
+            return resultado;
+        }
+    }
+}
